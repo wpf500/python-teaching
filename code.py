a7444bf593c7aadf48b874492706f40a2cc79ec6
Will Franklin <will.franklin@guardian.co.uk>
2016-07-15 15:23:32 +0100

    New file

--- code.py
+++ code.py
@@ -7,16 +7,16 @@ city_locations = {
 	'Dayr Az Zawr': {'lat': 35.329652, 'lon': 40.135034}
 }
 
-rows = list(csv.DictReader(open('data.csv')))
+raids = list(csv.DictReader(open('data.csv')))
 
-def get_row(n):
-	return rows[n - 1]
+def get_raid(n):
+	return raids[n - 1]
 
-def get_row_field(row, field):
-	return row[field]
+def get_raid_field(raid, field):
+	return raid[field]
 
-def get_row_location(row):
-	return {'lat': row['lat'], 'lon': row['lon']}
+def get_raid_location(raid):
+	return {'lat': raid['lat'], 'lon': raid['lon']}
 
 def get_city_location(city):
 	return city_locations[city]
@@ -24,10 +24,10 @@ def get_city_location(city):
 def get_distance(location1, location2):
 	d = 0
 	R = 6371e3 # metres
-	loc1_lat = math.radians(location1['lat'])
-	loc2_lat = math.radians(location2['lat'])
-	lat_d = math.radians(location2['lat']-location1['lat'])
-	lon_d = math.radians(location2['lon']-location1['lon'])
+	loc1_lat = math.radians(float(location1['lat']))
+	loc2_lat = math.radians(float(location2['lat']))
+	lat_d = math.radians(float(location2['lat'])-float(location1['lat']))
+	lon_d = math.radians(float(location2['lon'])-float(location1['lon']))
 
 	a1 = math.sin(lat_d/2) * math.sin(lat_d/2)
 	a2 = math.cos(loc1_lat) * math.cos(loc2_lat) * math.sin(lon_d/2) * math.sin(lon_d/2)
@@ -37,5 +37,5 @@ def get_distance(location1, location2):
 	d = R * c;
 	return d
 
-def get_rows():
-	return rows
+def get_raids():
+	return raids
